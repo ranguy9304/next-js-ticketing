cc2e26767ef93558a6a364910485f53cecd278ef
Rudra Patel <ranguy.yt@gmail.com>
2023-12-12 12:04:29 +0530

    server side fetch call error fix try

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+
+    experimental:{
+        appDir:true,
+        serverComponentsExternalPackages:["mongoose"],
+
+    },
+    
+}
+
+
 
 module.exports = nextConfig
